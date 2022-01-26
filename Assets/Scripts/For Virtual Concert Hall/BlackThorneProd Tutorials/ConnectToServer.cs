24ad9e25de47504d307f605ebb33daee18e3e3a0
Jailo26 <jaimelorenzo.cunanan@gmail.com>
2022-01-26 20:44:49 +0800

    Login - Lobby - Game Scene = Semi okay but its working

--- Assets/Scripts/For Virtual Concert Hall/BlackThorneProd Tutorials/ConnectToServer.cs
+++ Assets/Scripts/For Virtual Concert Hall/BlackThorneProd Tutorials/ConnectToServer.cs
@@ -1,27 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEngine.UI;
+
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+    //[SerializeField]
+   // private FirebaseManager connector;
+
     public TMP_InputField ingameNameInput;
     public TextMeshProUGUI buttonText;
     // Start is called before the first frame update
+
+    public void Awake()
+    {
+        //connector.gameObject.GetComponent<FirebaseManager>();
+    }
+
     public void OnClickConnect()
     {
-        if(ingameNameInput.text.Length >= 1)
-        {
+            buttonText.text = "Connecting...";
+            Debug.Log("Connecting...");
             PhotonNetwork.NickName = ingameNameInput.text;
             PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.AutomaticallySyncScene = true;
-            buttonText.text = "Connecting...";
-            Debug.Log("Connecting...");
-        }
-        
     }
 
     public override void OnConnectedToMaster()
